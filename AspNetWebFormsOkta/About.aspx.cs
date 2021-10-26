e5cb587b6bffa6049c3b9c05295440f4f531ca79
Fuji <fuji.nguyen@workcontrol.com>
2021-10-25 23:05:05 -0400

    Add project files.

---
+++ AspNetWebFormsOkta/About.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNetWebFormsOkta
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ClaimsPrincipal cp = Page.User as ClaimsPrincipal;
+            var claims = cp.Claims.ToList();
+        }
+    }
+}
---
+++ AspNetWebFormsOkta/Site.Master.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNetWebFormsOkta
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.Current.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+        {
+            Request.GetOwinContext().Authentication.SignOut();
+        }
+    }
+}
